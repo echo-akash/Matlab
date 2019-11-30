6fcd079e4b9a78490244ed310b0a8b6a86d98e0a
akash poddar <akash.poddar.0799@gmail.com>
2019-11-30 19:19:46 +0600

    Create Manchester.m

---
+++ Manchester.m
@@ -0,0 +1,16 @@
+m = [0 1 0 0 1 1];
+n = length(m);
+x = [];
+y = [];
+
+for i=1:n
+    x=[x i-1 i-1+0.5 i-1+0.5 i];
+    if(m(i)==0)
+        y=[y 1 1 -1 -1];
+    else
+        y=[y -1 -1 1 1];
+    end
+end
+
+plot(x,y),axis([0,n,-2,2]);
+title('Manchester');
